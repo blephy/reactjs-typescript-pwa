41a212f9a9294abbeca76e0c42a84d0a7517d1e1
Allan Dollé <dolle.allan@gmail.com>
2020-09-30 00:35:01 +0200

    chore(lighthouse): use tempory public storage and add url

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,11 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:3001/'],
+      url: ['http://localhost:3001/', 'http://localhost:3001/not-found'],
       startServerCommand: 'npm run start:prod'
+    },
+    upload: {
+      target: 'temporary-public-storage'
     },
     assert: {
       preset: 'lighthouse:no-pwa',
