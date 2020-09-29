527512ef431c32d631a6a43030676f3c90a2a18d
Allan Dollé <dolle.allan@gmail.com>
2020-09-29 21:05:44 +0200

    ci: make lighthouse more restrictive

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,9 +7,6 @@ module.exports = {
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
-        'uses-rel-preload': 'off',
-        'uses-rel-preconnect': 'off',
-        'render-blocking-resources': 'off',
         'robots-txt': 'off'
       }
     }
