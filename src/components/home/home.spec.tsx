5e8c46ad9fe9c574a44ef6d659968fb3f64fa189
Allan Dollé <dolle.allan@gmail.com>
2020-09-30 19:21:26 +0200

    feat: add css module test and share css variables

--- src/components/home/home.spec.tsx
+++ src/components/home/home.spec.tsx
@@ -2,6 +2,7 @@ import { shallow } from 'enzyme'
 import React from 'react'
 
 import Home from '.'
+import styles from './home.module.scss'
 
 describe('Home', () => {
   it('should render', () => {
@@ -14,5 +15,12 @@ describe('Home', () => {
     const wrapper = shallow(<Home />)
 
     expect(wrapper.find('h1')).toHaveLength(1)
+  })
+
+  it('should style the h1 element', () => {
+    const wrapper = shallow(<Home />)
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(wrapper.find(styles.title).exists).toBeTruthy()
   })
 })
--- src/components/loader/loader.spec.tsx
+++ src/components/loader/loader.spec.tsx
@@ -2,11 +2,26 @@ import { shallow } from 'enzyme'
 import React from 'react'
 
 import Loader from '.'
+import styles from './loader.module.scss'
 
 describe('Loader', () => {
   it('should render', () => {
     const wrapper = shallow(<Loader />)
 
     expect(wrapper.exists()).toBeTruthy()
+  })
+
+  it('should include a styled container', () => {
+    const wrapper = shallow(<Loader />)
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(wrapper.find(styles.loaderContainer).exists).toBeTruthy()
+  })
+
+  it('should include a styled loader', () => {
+    const wrapper = shallow(<Loader />)
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(wrapper.find(styles.loader).exists).toBeTruthy()
   })
 })
