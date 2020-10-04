74e089c39f548497d008f866d06d364bc717cdcf
Allan Dollé <dolle.allan@gmail.com>
2020-10-04 02:48:49 +0200

    ci: only warn about maskable-icon-audit

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -8,7 +8,10 @@ module.exports = {
       target: 'temporary-public-storage'
     },
     assert: {
-      preset: 'lighthouse:recommended'
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'maskable-icon-audit': 'warn'
+      }
     }
   }
 }
