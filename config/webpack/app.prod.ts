dc3069b467455e8b284c7b643acba07bb3d8de34
Allan Dollé <dolle.allan@gmail.com>
2020-10-04 03:16:00 +0200

    chore: support only modern browsers in production

--- config/webpack/app.prod.ts
+++ config/webpack/app.prod.ts
@@ -253,10 +253,7 @@ module.exports = {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true
-          }
+          loader: 'babel-loader'
         }
       },
       {
