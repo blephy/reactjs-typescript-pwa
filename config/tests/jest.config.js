ff149dd378f7f1da708f17d4c2acde201681808a
Allan Dollé <dolle.allan@gmail.com>
2020-10-05 22:37:36 +0200

    test: fix jest unit reporter configuration

--- config/tests/jest.config.js
+++ config/tests/jest.config.js
@@ -33,5 +33,6 @@ module.exports = {
     '!<rootDir>/src/**/*.d.ts',
     '!<rootDir>/src/**/*.spec.ts',
     '!<rootDir>/src/**/*.test.ts'
-  ]
+  ],
+  testResultsProcessor: 'jest-sonar-reporter'
 }
