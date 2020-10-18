75f0d389f17ab87072beeda56667f49c0ceb46de
Allan Dollé <hello@allandolle.fr>
2020-10-18 19:24:12 +0200

    fix: fix lhci

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -4,7 +4,7 @@ module.exports = {
       numberOfRuns: 5,
       url: ['http://localhost:3001/', 'http://localhost:3001/404'],
       startServerCommand: 'npm run start:prod',
-      startServerReadyPattern: '/Listening on port:/'
+      startServerReadyPattern: 'Listening on port:'
     },
     upload: {
       target: 'temporary-public-storage'
